89a6ba34287ba0292f6e0effffdb1e57decb5c60
nasoki <will.98.black@hotmail.com>
2023-11-20 15:15:19 +0300

    icicle1 complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 60;
         if (instance == null)
         {
             instance = this;
